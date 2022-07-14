2f006ffda27513aae568c9529fb2eaf85ae29418
21cloudocean <437508630@qq.com>
2022-07-14 16:29:47 +0800

    fetch product - get unique query param

--- product.js
+++ product.js
@@ -1,9 +1,19 @@
 const productDOM = document.querySelector(".product");
-const url =
-  "https://course-api.com/javascript-store-single-product?id=rec43w3ipXvP28vog";
+const url = "https://course-api.com/javascript-store-single-product";
 
 const fetchProduct = async () => {
-  return "product";
+  try {
+    productDOM.innerHTML = `<h4 class="product-loading">Loading...</h4>`;
+    // console.log(window.location.search);
+    const params = new URLSearchParams(window.location.search);
+    const id = params.get("id");
+    // console.log(id);
+    const response = await fetch(`${url}?id=${id}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    productDOM.innerHTML = `<p class="error">There was an error!</p>`;
+  }
 };
 
 const displayProduct = (product) => {
